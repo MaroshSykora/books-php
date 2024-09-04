40326507afca277a664ecda772cd5117dd26c0b5
Marosh Sykora <sykoramaros@gmail.com>
2024-09-04 09:35:42 +0200

    vytvoreni uvodni stranky index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// UVODNI STRANKA
 
 ?>
 
@@ -10,14 +11,45 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./scss/custom.css" rel="stylesheet">
   <title>Prehled knih</title>
+  <style>
+    /* body * {
+      border: solid 2px blue;
+    } */
+  </style>
 </head>
 
 <body>
+  <!-- navbar zacatek -->
   <section>
-    
-
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Books</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Add</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">List</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page">Search</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </section>
+  <!-- navbar konec -->
 
+  <!-- main zacatek -->
+  <section>
+    <span class="mainTitle">BOOKS</span>
+  </section>
+  <!-- main konec -->
 
 
   <!-- SKRIPT BOOTSTRAP -->
