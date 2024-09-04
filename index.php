6b4cf90a47a45572a3ef5e17b87fddf5017e9c87
Marosh Sykora <sykoramaros@gmail.com>
2024-09-04 09:05:34 +0200

    naimportovany bootstrap do projektu

--- index.php
+++ index.php
@@ -8,11 +8,15 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link href="/scss/custom.css rel=stylesheet">
+  <title>Prehled knih</title>
 </head>
 
 <body>
 
+  <!-- SKRIPT BOOTSTRAP -->
+  <script src="./bootstrap.bundle.min.js"></script>
+  <!-- ^^^^^ -->
 </body>
 
 </html>
