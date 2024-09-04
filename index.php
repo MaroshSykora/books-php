4fe6bbf3b93a384a7b75e90bdbf0cc58e11261db
Marosh Sykora <sykoramaros@gmail.com>
2024-09-04 09:15:17 +0200

    opraveny stylesheet odkaz v index.php souboru

--- index.php
+++ index.php
@@ -8,11 +8,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/scss/custom.css rel=stylesheet">
+  <link href="./scss/custom.css" rel="stylesheet">
   <title>Prehled knih</title>
 </head>
 
 <body>
+  <section>
+    
+
+  </section>
+
+
 
   <!-- SKRIPT BOOTSTRAP -->
   <script src="./bootstrap.bundle.min.js"></script>
