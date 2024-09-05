50ba733a38eab2cb98d523d48a5b8e7c40ec04ee
Marosh Sykora <sykoramaros@gmail.com>
2024-09-05 19:10:07 +0200

    upravene scss styly

--- add.php
+++ add.php
@@ -43,22 +43,22 @@ if (isset($_POST['add'])) {
 <body>
   <!-- navbar zacatek -->
   <section>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg black-bg">
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Books</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand green-color fs-3" href="index.php">Books</a>
+        <button class="navbar-toggler light-bg" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-4">
             <li class="nav-item">
-              <a class="nav-link active" href="add.php">Add</a>
+              <a class="nav-link active text-white" href="add.php">Add</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="list.php">List</a>
+              <a class="nav-link text-white" href="list.php">List</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="search.php">Search</a>
+              <a class="nav-link text-white" href="search.php">Search</a>
             </li>
           </ul>
         </div>
